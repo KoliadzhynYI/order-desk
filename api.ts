53222386435a1b507a9b901b44781f91ef3c5d06
Yurri Koliadzhyn <y.koliadzhyn@apiko.com>
2023-05-10 18:02:12 +0300

    added comments

--- api.ts
+++ api.ts
@@ -1,11 +1,13 @@
 import axios from "axios";
+// can be get from .env file
 const storeId = "53642";
 const apiKey = "wgLNjnE8zLZwPQXFeC7mPcEkdh3ripk5WvemgBFQp2yXzFjvRx";
 axios.defaults.baseURL = "https://app.orderdesk.me/api/v2";
+
 // main function that fires with crone job
 export const getHourlyOrders = async () => {
   let allOrders: any[] = [];
-  // date now but hard code for testing time when cron probably start "May 10, 2023 18:01:18"
+  // date we should get now, but hard code for testing time when cron probably start "May 10, 2023 18:01:18"
   const date = new Date("May 10, 2023 18:01:18");
   // can be more readable using momentjs
   const search_start_date = new Date(
@@ -46,10 +48,10 @@ export const getHourlyOrders = async () => {
       const orders = data.orders;
       allOrders.push(...orders);
       // to know if we should take more
-      const nextPage = data.total_records > initialTaken;
-      const newLimit = offset + limit;
-      if (nextPage) {
-        await getAllOrders(newLimit, limit);
+      const isNextPage = data.total_records > initialTaken;
+      const newOffset = offset + limit;
+      if (isNextPage) {
+        await getAllOrders(newOffset, limit);
       }
     } catch (error) {
       console.error("Error retrieving orders:", error);
